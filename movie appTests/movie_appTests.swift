c7d2f105c8b70fe84738f9bfbb9aa98d30da4ad5
iAutoparkCars <bonillasc@gmail.com>
2016-02-06 23:04:01 -0500

    Initial Commit

---
+++ movie appTests/movie_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  movie_appTests.swift
+//  movie appTests
+//
+//  Created by App Dev on 2/6/16.
+//  Copyright © 2016 App Dev. All rights reserved.
+//
+
+import XCTest
+@testable import movie_app
+
+class movie_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
